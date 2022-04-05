4f1425184c3000e692d3bff593bdbc66c8319233
Sulman <shahsuliman@gmail.com>
2022-04-05 23:15:51 +0500

    Added typeorm to the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule,TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
